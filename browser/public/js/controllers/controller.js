31f801d5e97a444c61b76360b0707fab1424c645
cnbates <cnbates19@gmail.com>
2013-11-11 16:37:11 -0700

    added things for the view

---
+++ browser/public/js/controllers/controller.js
@@ -0,0 +1,29 @@
+GutenbergsBox.ApplicationController = Ember.Controller.extend();
+
+GutenbergsBox.BookshelfController = Ember.ArrayController.createWithMixins({
+    content: [],
+    addItem: function(item) {
+        this.addObject(item);
+    }
+});
+
+GutenbergsBox.selectedUserController = Ember.ObjectController.createWithMixins({
+	selectedUser: [],
+	select: function(item) {
+		this.set('selectedUser', item);
+	}
+});
+
+GutenbergsBox.selectedBoxController = Ember.ObjectController.createWithMixins({
+	selectedBox: [],
+	select: function(item) {
+		this.set('selectedBox', item);
+	}
+});
+
+GutenbergsBox.selectedBookController = Ember.ObjectController.createWithMixins({
+	selectedBook: [],
+	select: function(item) {
+		this.set('selectedBook', item);
+	}
+});
