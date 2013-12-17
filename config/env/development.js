184401c841121e48eb2a9d3fcc77c217126238dd
Jon Kunkee <jonathan.kunkee@gmail.com>
2013-12-16 17:56:31 -0700

    Changed development port callbacks to reflect dev port

--- config/env/development.js
+++ config/env/development.js
@@ -1,27 +1,27 @@
 module.exports = {
     db: "mongodb://localhost/mean-dev",
-    port: 3000,
+    port: 35337,
     app: {
         name: "Gutenberg's Box - Development"
     },
     facebook: {
         clientID: "218491238333832",
         clientSecret: "0b1e2718e2e01160f428c52bbe4a24f7",
-        callbackURL: "http://localhost:3000/auth/facebook/callback"
+        callbackURL: "http://localhost:35337/auth/facebook/callback"
     },
     twitter: {
         clientID: "CONSUMER_KEY",
         clientSecret: "CONSUMER_SECRET",
-        callbackURL: "http://localhost:3000/auth/twitter/callback"
+        callbackURL: "http://localhost:35337/auth/twitter/callback"
     },
     github: {
         clientID: "6cd961294a1bf1177865",
         clientSecret: "100d8bdc3785f2a2ba2bd01f263b01367d2ff1fb",
-        callbackURL: "http://localhost:3000/auth/github/callback"
+        callbackURL: "http://localhost:35337/auth/github/callback"
     },
     google: {
         clientID: "APP_ID",
         clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/google/callback"
+        callbackURL: "http://localhost:35337/auth/google/callback"
     }
 }
